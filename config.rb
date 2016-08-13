cfaed6315b064cb27dba11967aeb21575070082a
F <fwrizalman@gmail.com>
2016-08-14 03:30:06 +0800

    Add pages

--- config.rb
+++ config.rb
@@ -1,10 +1,15 @@
-set :url_root, "http://railsgirls.my/"
+set :url_root, 'http://railsgirls.my/'
+set :blog_url, 'http://railsgirlsmy.herokuapp.com/'
+set :facebook_url, 'http://facebook.com/railsgirlskl'
+set :twitter_url, 'http://twitter.com/railsgirlskl'
+
 
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :partials_dir, 'partials'
 set :font_dir, 'fonts'
+set :relative_links, true
 
 page '/*.xml', layout: false
 page '/*.json', layout: false
@@ -12,6 +17,7 @@ page '/*.txt', layout: false
 
 configure :development do
   activate :livereload
+  activate :directory_indexes
 end
 
 configure :build do
